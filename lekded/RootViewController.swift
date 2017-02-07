fce10d30d8b0abc9801155e6932fd539abc65f99
kraiwut15 <kraiwut15@yahoo.com>
2017-02-07 14:35:54 +0700

    commit

---
+++ lekded/RootViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lekded
+//
+//  Created by Kraiwut on 18/1/60.
+//  Copyright © พ.ศ. 2560 SamartMutimedia. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ lekded/SMTopCell.swift
@@ -0,0 +1,24 @@
+//
+//  SMTopCell.swift
+//  lekded
+//
+//  Created by Kraiwut on 3/2/60.
+//  Copyright © พ.ศ. 2560 SamartMutimedia. All rights reserved.
+//
+
+import UIKit
+
+class SMTopCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
